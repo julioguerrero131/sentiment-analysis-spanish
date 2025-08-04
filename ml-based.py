34766a3c281413ebaa0b71049157fa991d14703b
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-08-04 16:54:49 -0500

    correcciones en las metricas

--- ml-based.py
+++ ml-based.py
@@ -1,6 +1,6 @@
+import pandas as pd
 from pysentimiento import create_analyzer
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -39,14 +39,17 @@ df["pred"] = df["text"].progress_apply(clasificar_sentimiento)
 df = df[df["pred"] != "ERROR"]
 
 # === Evaluación ===
-etiquetas_modelo = ["NEG", "NEU", "POS"]
-etiquetas_visuales = ["negativo", "neutro", "positivo"]
+etiquetas_modelo = ["POS", "NEU", "NEG"]
+etiquetas_visuales = ["Positivo", "Neutro", "Negativo"]
 
 # === Métricas ===
 print("\n=== MÉTRICAS DE CLASIFICACIÓN ===")
 
+# Matriz de Confusión
+cm = confusion_matrix(df["label"], df["pred"], labels=etiquetas_modelo)
+
 # Reporte completo
-reporte = classification_report(df["label"], df["pred"], labels=etiquetas_visuales, zero_division=0)
+reporte = classification_report(df["label"], df["pred"], labels=etiquetas_modelo, zero_division=0)
 print(reporte)
 
 # Métricas individuales
@@ -60,9 +63,7 @@ print(f"Precision: {precision:.4f}")
 print(f"Recall   : {recall:.4f}")
 print(f"F1-Score : {f1:.4f}")
 
-# Matriz de Confusión
-cm = confusion_matrix(df["label"], df["pred"], labels=etiquetas_modelo)
-
+# Visualización
 plt.figure(figsize=(6, 4))
 sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=etiquetas_visuales, yticklabels=etiquetas_visuales)
 plt.xlabel("Predicción")
